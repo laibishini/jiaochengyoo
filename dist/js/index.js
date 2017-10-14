e0b3b461c119617db124e2338178b9930c1941e0
huhao <674698095@qq.com>
2017-10-14 16:17:47 +0800

    首页没完成

---
+++ dist/js/index.js
@@ -0,0 +1,1 @@
+var a=1,c=0;window.onload=function(){console.log(2222222222)};
---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by jne on 2017/10/14.
+ */
+var a = 1;var c=0;
+
+
+window.onload = function(){
+    console.log(2222222222)
+}
